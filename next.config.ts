bc06b8e8a2ee28bba499bb98e33cd782bc94871c
Risna Dilfa <risnadilfa@gmail.com>
2026-02-24 07:30:49 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   async rewrites() {
